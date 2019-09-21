08405e11e10e33715f36a3e86b82e182a1345441
VictorChiangTW <45275788+VictorChiangTW@users.noreply.github.com>
2019-09-21 18:38:10 +0800

    add tqdm
    import tqdm to show the progress bar while merging

--- merging_pdf.py
+++ merging_pdf.py
@@ -1,25 +1,23 @@
 import PyPDF2
 import os
-
-
+import time
+# from progressbar import ProgressBar
+from tqdm import tqdm
 
 merge = PyPDF2.PdfFileMerger()
 filenames = os.listdir('w_m') #把想要合併的檔案放在「w_m」這個資料夾
 filenames.sort()
 print(filenames)
-for file in filenames:
-    print(file)
+for file in tqdm(filenames):
     if file.endswith('.pdf'):
         merge.append(PyPDF2.PdfFileReader(os.path.join('w_m', file)))
+
 merge.write(os.path.join('result', 'be_merged.pdf'))
+# 以上這個方法，沒辦法顯示出真正的執行進度
 
 
 
-# import os
-# path = "/Users/victorchiang/OneDrive/python/coding/pdf_project/w_m" #待讀取的資料夾
-# path_list = os.listdir(path)
-# path_list.sort() #對讀取的路徑進行排序
-# for filename in path_list:
-#     print(os.path.join(path, filename))
-
-# 以上是讓PDF按照順序讀取的方法
+# with progressbar.ProgressBar(max_value=100) as bar:
+#     for i in range(100):
+#         time.sleep(0.1)
+#         bar.update(i)
